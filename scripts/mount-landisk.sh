4d33b303b3b9a6423efe19da2e075d196a314d26
Shuhei Nitta <huisintheta@gmail.com>
2022-10-19 13:36:53 +0900

    Add mount-landisk.sh

---
+++ scripts/mount-landisk.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+set -euC
+set -o pipefail
+
+MOUNT_POINT="/mnt/landisk"
+SMBCREDS_FILEPATH="/root/smbcreds"
+LANDISK_IPADDRESS="192.168.0.10"
+
+if [ ! -d "$MOUNT_POINT" ]; then
+    sudo mkdir -p "$MOUNT_POINT"
+fi
+
+{
+    OPTIONS="credentials=$SMBCREDS_FILEPATH,nosuid,noexec,nodev,rw,vers=1.0"
+    echo "$LANDISK_IPADDRESS $MOUNT_POINT cifs $OPTIONS"    
+} | sudo tee -a /etc/fstab
