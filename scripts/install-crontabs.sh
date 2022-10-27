63d06f672ec4e5f36166d82edcbf95a93af79f77
Shuhei Nitta <huisintheta@gmail.com>
2022-10-27 17:40:02 +0900

    🎨 Add apt-upgrade.sh (#12)

--- scripts/install-crontabs.sh
+++ scripts/install-crontabs.sh
@@ -12,6 +12,13 @@ sudo chown root:root /etc/logrotate.d/cronlog.conf
 if [ ! -d /var/log/cron ]; then
     sudo mkdir -p /var/log/cron
 fi
+
+# wakealarm
 sudo cp "$SCRIPTS_DIR"/wakealarm.sh /usr/local/bin/
 sudo chmod 544 /usr/local/bin/wakealarm.sh
 sudo chown root:root /usr/local/bin/wakealarm.sh
+
+# apt upgrade
+sudo cp "$SCRIPTS_DIR"/apt-upgrade.sh /usr/local/bin/
+sudo chmod 544 /usr/local/bin/apt-upgrade.sh
+sudo chown root:root /usr/local/bin/apt-upgrade.sh
