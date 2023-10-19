5ec3713a80289ec1a3a2745c54db054ff3e93911
Shuhei Nitta <huisintheta@gmail.com>
2023-10-19 06:03:15 +0000

    Update apt-upgrade.sh

--- scripts/apt-upgrade.sh
+++ scripts/apt-upgrade.sh
@@ -4,10 +4,7 @@ set -euC
 set -o pipefail
 set -x
 
-UPGRADE_LOG_FILE="/var/log/cron/apt-upgrade.log"
-AUTOREMOVE_LOG_FILE="/var/log/cron/apt-autoremove.log"
-
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
-sudo apt-get upgrade -y | tee -a "$UPGRADE_LOG_FILE"
-sudo apt-get autoremove -y | tee -a "$AUTOREMOVE_LOG_FILE"
+sudo apt-get upgrade -y
+sudo apt-get autoremove -y
